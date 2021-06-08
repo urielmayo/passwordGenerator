a52a7a2887e306722891c52821079290fe296cc0
urielmayo <urimayo00@gmail.com>
2021-06-08 00:40:56 -0300

    adding password storage in 'files' directory

--- gen_password.py
+++ gen_password.py
@@ -1,5 +1,12 @@
+import os
 import random
 from string import ascii_uppercase, ascii_lowercase, punctuation
+
+#MACROS
+FILE_DIR = './files'
+RETRY = 'RETRY'
+SPECIAL_CHARS_ENABLED = 's_c_e'
+FILE_NAME_INPUT = 'f_n'
 
 def generate_password(length, special_chars = False):
     upper_letters = list(ascii_uppercase)
@@ -18,18 +25,65 @@ def generate_password(length, special_chars = False):
     
     return password
 
+def verify_input(input_type, file_name=''):
+    
+    if input_type == SPECIAL_CHARS_ENABLED:
+        answer = ' '
+
+        while (answer.upper() != 'N') and (answer.upper() != 'S'):
+            answer = input("usar caracteres especiales (s/n): ")
+            if (answer.upper() != 'N') and (answer.upper() != 'S'):
+                print('Respuesta no valida, intente de nuevo')
+        
+        if answer.upper() == 'S':
+            return True
+        else:
+            return False
+    
+    elif input_type == FILE_NAME_INPUT:
+        file_name = file_name
+        change_file = ''
+        if  (file_name+'.txt') in os.listdir(FILE_DIR):
+            while (change_file.upper() != 'N') and (change_file.upper() != 'S'):
+                change_file = input('El nombre de archivo ya existe, desea reemplazarlo? (s/n): ')
+                if (change_file.upper() != 'N') and (change_file.upper() != 'S'):
+                    print('Respuesta no valida, intente de nuevo')
+            
+            if change_file.upper() == 'S':
+                return file_name
+            else:
+                return RETRY
+        else:
+            return file_name
+
+def create_file(file_name, password):
+    with open(f'{FILE_DIR}/{file_name}.txt','w') as file:
+        file.write(password + '\n')
 
 def main():
+    if not 'files' in os.listdir():
+        os.mkdir(FILE_DIR)
+
+    while True:
+        file_name = input("Nombre : ")
+        file_name = verify_input(FILE_NAME_INPUT,file_name=file_name)
+        if file_name != RETRY:
+            break
+        
     length = int(input("ingresar largo de la contraseña: "))
-    special_chars_enabled = input("usar caracteres especiales (s/n): ")
+    special_chars_enabled = verify_input(SPECIAL_CHARS_ENABLED)
     
+    '''
     if special_chars_enabled == 's':
         special_chars_enabled = True
     else:
         special_chars_enabled = False
-        
+    '''    
     password = generate_password(length, special_chars_enabled)
-    print(password)
+    print(f'''new password created: {password}, 
+    saving it in {file_name}.txt''')
+
+    create_file(file_name, password)
 
 
 if __name__ == '__main__':
